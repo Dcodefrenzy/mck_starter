bfd3153b33efbeefc2fea30ff7449c0f0c9d1bd4
Ayodeji <ayodejisamuelfakunle@gmail.com>
2022-01-15 23:00:24 +0100

    building somwthing small that can help people shop online without getting scammed

--- v1/routes/router.php
+++ v1/routes/router.php
@@ -26,12 +26,18 @@ if (count($uri) > 2) {
 }
   // $query_string = explode("?",$uri[1])[1];
   switch ($uri[1]) {
+    case '':
+      include APP_PATH. "/views/index.php";
+      break;
+    case 'index':
+      include APP_PATH. "/views/index.php";
+      break;
     case 'test':
-    include APP_PATH."/views/test.php";
-    break;
+      include APP_PATH."/views/test.php";
+      break;
     case 'test?'.$query_string:
-    include APP_PATH."/views/test.php";
-    break;
+      include APP_PATH."/views/test.php";
+      break;
   }
 
 }
---
+++ v1/views/includes/header.php
@@ -0,0 +1,16 @@
+
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from codeminifier.com/job-stock-5.4.1/job-stock/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 14 Oct 2021 06:58:47 GMT -->
+<head>
+    <!-- Basic Page Needs==================================================-->
+    <title>Job Stock - Responsive Job Portal Bootstrap Template | ThemezHub</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- CSS==================================================-->
+    <link rel="stylesheet" href="/client/assets/plugins/css/plugins.css">
+    <link href="/client/assets/css/style.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" id="jssDefault" href="/client/assets/css/colors/green-style.css"> 
+</head>
